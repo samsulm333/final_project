0287a32dae08a3e92d3a19797e36e164ad37b82b
samsulm333 <66301391+samsulm333@users.noreply.github.com>
2026-06-26 14:13:12 +0700

    Update UserSeeder.php
    seeder tambah 50 users

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -10,301 +10,303 @@ class UserSeeder extends Seeder
 {
     public function run(): void
     {
-       User::create([
-            'name' => 'Calon Siswa Demo_2',
-            'email' => 'siswa_002@ppdb.com',
-            'password' => Hash::make('password123'),
-            'role' => 'siswa'
-        ]);
-		User::create([
+		User::insert([
+				[
+					'name' => 'Calon Siswa Demo_2',
+				    'email' => 'siswa_002@ppdb.com',
+				    'password' => Hash::make('password123'),
+				    'role' => 'siswa'
+				],
+				[
 					'name' => 'Calon Siswa Demo_3',
-					'email' => 'siswa_003@ppdb.com',
-					'password' => Hash::make('password123'),
-					'role' => 'siswa'
-				]);
-		User::create([
+				    'email' => 'siswa_013@ppdb.com',
+				    'password' => Hash::make('password123'),
+				    'role' => 'siswa'
+				],
+				[
 					'name' => 'Calon Siswa Demo_4',
-					'email' => 'siswa_004@ppdb.com',
-					'password' => Hash::make('password123'),
-					'role' => 'siswa'
-				]);
-		User::create([
+				    'email' => 'siswa_024@ppdb.com',
+				    'password' => Hash::make('password123'),
+				    'role' => 'siswa'
+				],
+				[
 					'name' => 'Calon Siswa Demo_5',
-					'email' => 'siswa_005@ppdb.com',
-					'password' => Hash::make('password123'),
-					'role' => 'siswa'
-				]);
-		User::create([
+				    'email' => 'siswa_035@ppdb.com',
+				    'password' => Hash::make('password123'),
+				    'role' => 'siswa'
+				],
+				[
 					'name' => 'Calon Siswa Demo_6',
-					'email' => 'siswa_006@ppdb.com',
-					'password' => Hash::make('password123'),
-					'role' => 'siswa'
-				]);
-		User::create([
+				    'email' => 'siswa_046@ppdb.com',
+				    'password' => Hash::make('password123'),
+				    'role' => 'siswa'
+				],
+				[
 					'name' => 'Calon Siswa Demo_7',
-					'email' => 'siswa_007@ppdb.com',
-					'password' => Hash::make('password123'),
-					'role' => 'siswa'
-				]);
-		User::create([
+				    'email' => 'siswa_057@ppdb.com',
+				    'password' => Hash::make('password123'),
+				    'role' => 'siswa'
+				],
+				[
 					'name' => 'Calon Siswa Demo_8',
-					'email' => 'siswa_008@ppdb.com',
-					'password' => Hash::make('password123'),
-					'role' => 'siswa'
-				]);
-		User::create([
+				    'email' => 'siswa_068@ppdb.com',
+				    'password' => Hash::make('password123'),
+				    'role' => 'siswa'
+				],
+				[
 					'name' => 'Calon Siswa Demo_9',
-					'email' => 'siswa_009@ppdb.com',
-					'password' => Hash::make('password123'),
-					'role' => 'siswa'
-				]);
-		User::create([
+				    'email' => 'siswa_079@ppdb.com',
+				    'password' => Hash::make('password123'),
+				    'role' => 'siswa'
+				],
+				[ 
 					'name' => 'Calon Siswa Demo_10',
-					'email' => 'siswa_0010@ppdb.com',
-					'password' => Hash::make('password123'),
-					'role' => 'siswa'
-				]);
-		User::create([
+				    'email' => 'siswa_0810@ppdb.com',
+				    'password' => Hash::make('password123'),
+				    'role' => 'siswa'
+				],
+				[
 					'name' => 'Calon Siswa Demo_11',
-					'email' => 'siswa_0011@ppdb.com',
-					'password' => Hash::make('password123'),
-					'role' => 'siswa'
-				]);
-		User::create([
+				    'email' => 'siswa_0911@ppdb.com',
+				    'password' => Hash::make('password123'),
+				    'role' => 'siswa'
+				],
+				[
 					'name' => 'Calon Siswa Demo_12',
-					'email' => 'siswa_0012@ppdb.com',
-					'password' => Hash::make('password123'),
-					'role' => 'siswa'
-				]);
-		User::create([
+				    'email' => 'siswa_1012@ppdb.com',
+				    'password' => Hash::make('password123'),
+				    'role' => 'siswa'
+				],
+				[
 					'name' => 'Calon Siswa Demo_13',
-					'email' => 'siswa_0013@ppdb.com',
-					'password' => Hash::make('password123'),
-					'role' => 'siswa'
-				]);
-		User::create([
+				    'email' => 'siswa_1113@ppdb.com',
+				    'password' => Hash::make('password123'),
+				    'role' => 'siswa'
+				],
+				[
 					'name' => 'Calon Siswa Demo_14',
-					'email' => 'siswa_0014@ppdb.com',
-					'password' => Hash::make('password123'),
-					'role' => 'siswa'
-				]);
-		User::create([
+				    'email' => 'siswa_1214@ppdb.com',
+				    'password' => Hash::make('password123'),
+				    'role' => 'siswa'
+				],
+				[
 					'name' => 'Calon Siswa Demo_15',
-					'email' => 'siswa_0015@ppdb.com',
-					'password' => Hash::make('password123'),
-					'role' => 'siswa'
-				]);
-		User::create([
+				    'email' => 'siswa_1315@ppdb.com',
+				    'password' => Hash::make('password123'),
+				    'role' => 'siswa'
+				],
+				[
 					'name' => 'Calon Siswa Demo_16',
-					'email' => 'siswa_0016@ppdb.com',
-					'password' => Hash::make('password123'),
-					'role' => 'siswa'
-				]);
-		User::create([
+				    'email' => 'siswa_1416@ppdb.com',
+				    'password' => Hash::make('password123'),
+				    'role' => 'siswa'
+				],
+				[
 					'name' => 'Calon Siswa Demo_17',
-					'email' => 'siswa_0017@ppdb.com',
-					'password' => Hash::make('password123'),
-					'role' => 'siswa'
-				]);
-		User::create([
+				    'email' => 'siswa_1517@ppdb.com',
+				    'password' => Hash::make('password123'),
+				    'role' => 'siswa'
+				],
+				[
 					'name' => 'Calon Siswa Demo_18',
-					'email' => 'siswa_0018@ppdb.com',
-					'password' => Hash::make('password123'),
-					'role' => 'siswa'
-				]);
-		User::create([
+				    'email' => 'siswa_1618@ppdb.com',
+				    'password' => Hash::make('password123'),
+				    'role' => 'siswa'
+				],
+				[
 					'name' => 'Calon Siswa Demo_19',
-					'email' => 'siswa_0019@ppdb.com',
-					'password' => Hash::make('password123'),
-					'role' => 'siswa'
-				]);
-		User::create([
+				    'email' => 'siswa_1719@ppdb.com',
+				    'password' => Hash::make('password123'),
+				    'role' => 'siswa'
+				],
+				[
 					'name' => 'Calon Siswa Demo_20',
-					'email' => 'siswa_0020@ppdb.com',
-					'password' => Hash::make('password123'),
-					'role' => 'siswa'
-				]);
-		User::create([
+				    'email' => 'siswa_1820@ppdb.com',
+				    'password' => Hash::make('password123'),
+				    'role' => 'siswa'
+				],
+				[
 					'name' => 'Calon Siswa Demo_21',
-					'email' => 'siswa_0021@ppdb.com',
-					'password' => Hash::make('password123'),
-					'role' => 'siswa'
-				]);
-		User::create([
+				    'email' => 'siswa_1921@ppdb.com',
+				    'password' => Hash::make('password123'),
+				    'role' => 'siswa'
+				],
+				[
 					'name' => 'Calon Siswa Demo_22',
-					'email' => 'siswa_0022@ppdb.com',
-					'password' => Hash::make('password123'),
-					'role' => 'siswa'
-				]);
-		User::create([
+				    'email' => 'siswa_2022@ppdb.com',
+				    'password' => Hash::make('password123'),
+				    'role' => 'siswa'
+				],
+				[
 					'name' => 'Calon Siswa Demo_23',
-					'email' => 'siswa_0023@ppdb.com',
-					'password' => Hash::make('password123'),
-					'role' => 'siswa'
-				]);
-		User::create([
+				    'email' => 'siswa_2123@ppdb.com',
+				    'password' => Hash::make('password123'),
+				    'role' => 'siswa'
+				],
+				[
 					'name' => 'Calon Siswa Demo_24',
-					'email' => 'siswa_0024@ppdb.com',
-					'password' => Hash::make('password123'),
-					'role' => 'siswa'
-				]);
-		User::create([
+				    'email' => 'siswa_2224@ppdb.com',
+				    'password' => Hash::make('password123'),
+				    'role' => 'siswa'
+				],
+				[
 					'name' => 'Calon Siswa Demo_25',
-					'email' => 'siswa_0025@ppdb.com',
-					'password' => Hash::make('password123'),
-					'role' => 'siswa'
-				]);
-		User::create([
+				    'email' => 'siswa_2325@ppdb.com',
+				    'password' => Hash::make('password123'),
+				    'role' => 'siswa'
+				],
+				[
 					'name' => 'Calon Siswa Demo_26',
-					'email' => 'siswa_0026@ppdb.com',
-					'password' => Hash::make('password123'),
-					'role' => 'siswa'
-				]);
-		User::create([
+				    'email' => 'siswa_2426@ppdb.com',
+				    'password' => Hash::make('password123'),
+				    'role' => 'siswa'
+				],
+				[
 					'name' => 'Calon Siswa Demo_27',
-					'email' => 'siswa_0027@ppdb.com',
-					'password' => Hash::make('password123'),
-					'role' => 'siswa'
-				]);
-		User::create([
+				    'email' => 'siswa_2527@ppdb.com',
+				    'password' => Hash::make('password123'),
+				    'role' => 'siswa'
+				],
+				[
 					'name' => 'Calon Siswa Demo_28',
-					'email' => 'siswa_0028@ppdb.com',
-					'password' => Hash::make('password123'),
-					'role' => 'siswa'
-				]);
-		User::create([
+				    'email' => 'siswa_2628@ppdb.com',
+				    'password' => Hash::make('password123'),
+				    'role' => 'siswa'
+				],
+				[   
 					'name' => 'Calon Siswa Demo_29',
-					'email' => 'siswa_0029@ppdb.com',
-					'password' => Hash::make('password123'),
-					'role' => 'siswa'
-				]);
-		User::create([
+				    'email' => 'siswa_2729@ppdb.com',
+				    'password' => Hash::make('password123'),
+				    'role' => 'siswa'
+				],
+				[
 					'name' => 'Calon Siswa Demo_30',
-					'email' => 'siswa_0030@ppdb.com',
-					'password' => Hash::make('password123'),
-					'role' => 'siswa'
-				]);
-		User::create([
+				    'email' => 'siswa_2830@ppdb.com',
+				    'password' => Hash::make('password123'),
+				    'role' => 'siswa'
+				],
+				[
 					'name' => 'Calon Siswa Demo_31',
-					'email' => 'siswa_0031@ppdb.com',
-					'password' => Hash::make('password123'),
-					'role' => 'siswa'
-				]);
-		User::create([
+				    'email' => 'siswa_2931@ppdb.com',
+				    'password' => Hash::make('password123'),
+				    'role' => 'siswa'
+				],
+				[
 					'name' => 'Calon Siswa Demo_32',
-					'email' => 'siswa_0032@ppdb.com',
-					'password' => Hash::make('password123'),
-					'role' => 'siswa'
-				]);
-		User::create([
+				    'email' => 'siswa_3032@ppdb.com',
+				    'password' => Hash::make('password123'),
+				    'role' => 'siswa'
+				],
+				[
 					'name' => 'Calon Siswa Demo_33',
-					'email' => 'siswa_0033@ppdb.com',
-					'password' => Hash::make('password123'),
-					'role' => 'siswa'
-				]);
-		User::create([
+				    'email' => 'siswa_3133@ppdb.com',
+				    'password' => Hash::make('password123'),
+				    'role' => 'siswa'
+				],
+				[
 					'name' => 'Calon Siswa Demo_34',
-					'email' => 'siswa_0034@ppdb.com',
-					'password' => Hash::make('password123'),
-					'role' => 'siswa'
-				]);
-		User::create([
+				    'email' => 'siswa_3234@ppdb.com',
+				    'password' => Hash::make('password123'),
+				    'role' => 'siswa'
+				],
+				[
 					'name' => 'Calon Siswa Demo_35',
-					'email' => 'siswa_0035@ppdb.com',
-					'password' => Hash::make('password123'),
-					'role' => 'siswa'
-				]);
-		User::create([
+				    'email' => 'siswa_3335@ppdb.com',
+				    'password' => Hash::make('password123'),
+				    'role' => 'siswa'
+				],
+				[
 					'name' => 'Calon Siswa Demo_36',
-					'email' => 'siswa_0036@ppdb.com',
-					'password' => Hash::make('password123'),
-					'role' => 'siswa'
-				]);
-		User::create([
+				    'email' => 'siswa_3436@ppdb.com',
+				    'password' => Hash::make('password123'),
+				    'role' => 'siswa'
+				],
+				[ 
 					'name' => 'Calon Siswa Demo_37',
-					'email' => 'siswa_0037@ppdb.com',
-					'password' => Hash::make('password123'),
-					'role' => 'siswa'
-				]);
-		User::create([
+				    'email' => 'siswa_3537@ppdb.com',
+				    'password' => Hash::make('password123'),
+				    'role' => 'siswa'
+				],
+				[
 					'name' => 'Calon Siswa Demo_38',
-					'email' => 'siswa_0038@ppdb.com',
-					'password' => Hash::make('password123'),
-					'role' => 'siswa'
-				]);
-		User::create([
+				    'email' => 'siswa_3638@ppdb.com',
+				    'password' => Hash::make('password123'),
+				    'role' => 'siswa'
+				],
+				[
 					'name' => 'Calon Siswa Demo_39',
-					'email' => 'siswa_0039@ppdb.com',
-					'password' => Hash::make('password123'),
-					'role' => 'siswa'
-				]);
-		User::create([
+				    'email' => 'siswa_3739@ppdb.com',
+				    'password' => Hash::make('password123'),
+				    'role' => 'siswa'
+				],
+				[
 					'name' => 'Calon Siswa Demo_40',
-					'email' => 'siswa_0040@ppdb.com',
-					'password' => Hash::make('password123'),
-					'role' => 'siswa'
-				]);
-		User::create([
+				    'email' => 'siswa_3840@ppdb.com',
+				    'password' => Hash::make('password123'),
+				    'role' => 'siswa'
+				],
+				[
 					'name' => 'Calon Siswa Demo_41',
-					'email' => 'siswa_0041@ppdb.com',
-					'password' => Hash::make('password123'),
-					'role' => 'siswa'
-				]);
-		User::create([
+				    'email' => 'siswa_3941@ppdb.com',
+				    'password' => Hash::make('password123'),
+				    'role' => 'siswa'
+				], 
+				[
 					'name' => 'Calon Siswa Demo_42',
-					'email' => 'siswa_0042@ppdb.com',
-					'password' => Hash::make('password123'),
-					'role' => 'siswa'
-				]);
-		User::create([
+				    'email' => 'siswa_4042@ppdb.com',
+				    'password' => Hash::make('password123'),
+				    'role' => 'siswa'
+				],
+				[
 					'name' => 'Calon Siswa Demo_43',
-					'email' => 'siswa_0043@ppdb.com',
-					'password' => Hash::make('password123'),
-					'role' => 'siswa'
-				]);
-		User::create([
+				    'email' => 'siswa_4143@ppdb.com',
+				    'password' => Hash::make('password123'),
+				    'role' => 'siswa'
+				],
+				[
 					'name' => 'Calon Siswa Demo_44',
-					'email' => 'siswa_0044@ppdb.com',
-					'password' => Hash::make('password123'),
-					'role' => 'siswa'
-				]);
-		User::create([
+				    'email' => 'siswa_4244@ppdb.com',
+				    'password' => Hash::make('password123'),
+				    'role' => 'siswa'
+				],
+				[
 					'name' => 'Calon Siswa Demo_45',
-					'email' => 'siswa_0045@ppdb.com',
-					'password' => Hash::make('password123'),
-					'role' => 'siswa'
-				]);
-		User::create([
+				    'email' => 'siswa_4345@ppdb.com',
+				    'password' => Hash::make('password123'),
+				    'role' => 'siswa'
+				],
+				[
 					'name' => 'Calon Siswa Demo_46',
-					'email' => 'siswa_0046@ppdb.com',
-					'password' => Hash::make('password123'),
-					'role' => 'siswa'
-				]);
-		User::create([
+				    'email' => 'siswa_4446@ppdb.com',
+				    'password' => Hash::make('password123'),
+				    'role' => 'siswa'
+				],
+				[
 					'name' => 'Calon Siswa Demo_47',
-					'email' => 'siswa_0047@ppdb.com',
-					'password' => Hash::make('password123'),
-					'role' => 'siswa'
-				]);
-		User::create([
+				    'email' => 'siswa_4547@ppdb.com',
+				    'password' => Hash::make('password123'),
+				    'role' => 'siswa'
+				],
+				[
 					'name' => 'Calon Siswa Demo_48',
-					'email' => 'siswa_0048@ppdb.com',
-					'password' => Hash::make('password123'),
-					'role' => 'siswa'
-				]);
-		User::create([
+				    'email' => 'siswa_4648@ppdb.com',
+				    'password' => Hash::make('password123'),
+				    'role' => 'siswa'
+				],
+				[
 					'name' => 'Calon Siswa Demo_49',
-					'email' => 'siswa_0049@ppdb.com',
-					'password' => Hash::make('password123'),
-					'role' => 'siswa'
-				]);
-		User::create([
+				    'email' => 'siswa_4749@ppdb.com',
+				    'password' => Hash::make('password123'),
+				    'role' => 'siswa'
+				],
+				[
 					'name' => 'Calon Siswa Demo_50',
-					'email' => 'siswa_0050@ppdb.com',
-					'password' => Hash::make('password123'),
-					'role' => 'siswa'
-				]);
-		
+				    'email' => 'siswa_4850@ppdb.com',
+				    'password' => Hash::make('password123'),
+				    'role' => 'siswa'
+				]
+			
+			]);	
     }
 }
 
