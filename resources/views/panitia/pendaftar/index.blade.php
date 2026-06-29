f0f74d2fba3353272f4d5879d0b057f60837ebd3
samsulm333 <66301391+samsulm333@users.noreply.github.com>
2026-06-29 16:21:45 +0700

    Update index.blade.php
    Revisi status diterima

--- resources/views/panitia/pendaftar/index.blade.php
+++ resources/views/panitia/pendaftar/index.blade.php
@@ -38,6 +38,8 @@
                                         <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-yellow-100 text-yellow-800">Menunggu</span>
                                     @elseif($reg->status === 'terverifikasi')
                                         <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">Terverifikasi</span>
+                                    @elseif($reg->status === 'diterima')
+                                        <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">diterima</span>
                                     @else
                                         <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-red-100 text-red-800">Ditolak</span>
                                     @endif
